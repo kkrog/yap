17e949c2fda4e6ed4a4b6bac0ddde23c9556e69f
kkrog <58795517+kkrog@users.noreply.github.com>
2020-03-23 19:38:11 -0500

    first commit

---
+++ src/components/Business/Business.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import React-DOM from 'react-dom';
+let business{
+    imageSrc: 'https://s3.amazonaws.com/codecademycontent/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90   
+};
+Class Business extends React.Component{
+    render(){
+
+    }
+}
+
+
+export default Business;
+//don't forget to change class to className for JSX formatting
